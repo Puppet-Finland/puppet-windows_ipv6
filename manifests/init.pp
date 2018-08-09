55ee9e1d0c730c55d26e9261f6070c36ac46f28d
Petri Lammi <petri.lammi@tietoteema.fi>
2018-08-09 20:35:22 +0300

    Fix possible values to align with dword type
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,14 +6,15 @@
 #                              Defaults to '0x20' to prefer IPv4 over IPv6 by changing entries in the prefix policy table
 #
 # Valid values are:
-# 0 to re-enable all IPv6 components (Windows default setting).
-# 0xff to disable all IPv6 components except the IPv6 loopback interface.
+# 0 (0)to re-enable all IPv6 components (Windows default setting).
+# 0xff (255) to disable all IPv6 components except the IPv6 loopback interface.
 # This value also configures Windows to prefer using IPv4 over IPv6 by changing entries in the prefix policy# table.
 # For more information, see Source and destination address selection.
-# 0x20 to prefer IPv4 over IPv6 by changing entries in the prefix policy table.
-# 0x10 to disable IPv6 on all nontunnel interfaces (both LAN and Point-to-Point Protocol [PPP] interfaces).
-# 0x01 to disable IPv6 on all tunnel interfaces. These include Intra-Site Automatic Tunnel Addressing Protocol (ISATAP), 6to4, and Teredo.
-# 0x11 to disable all IPv6 interfaces except for the IPv6 loopback interface.
+# 0x20 (32) to prefer IPv4 over IPv6 by changing entries in the prefix policy table.
+# 0x10 (16) to disable IPv6 on all nontunnel interfaces (both LAN and Point-to-Point Protocol [PPP] interfaces).
+# 0x01 (1) to disable IPv6 on all tunnel interfaces.
+# These include Intra-Site Automatic Tunnel Addressing Protocol (ISATAP), 6to4, and Teredo.
+# 0x11 (17) to disable all IPv6 interfaces except for the IPv6 loopback interface.
 # https://support.microsoft.com/en-us/kb/929852
 class windows_ipv6
 (
@@ -21,7 +22,7 @@ class windows_ipv6
 )
 {
 
-  unless $disabled_components_value in [ '0', '0xff', '0x20', '0x10', '0x01', '0x11', '32' ] {
+  unless $disabled_components_value in [ '0', '255', '32', '16', '1', '17' ] {
     fail("Invalid value ${disabled_components_value} for the DisabledComponents key")
   }
 
